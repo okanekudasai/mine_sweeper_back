d3c163a8f2263a06179f97e46137fc8b2b40de52
okane <okanekudasai6@gmail.com>
2024-01-11 16:34:46 +0900

    백준 자동푸쉬를 위한 코드 추가

--- src/main/java/com/example/minesweeper/controller/TempController.java
+++ src/main/java/com/example/minesweeper/controller/TempController.java
@@ -3,12 +3,15 @@ package com.example.minesweeper.controller;
 import com.example.minesweeper.dto.CodeDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin("*")
@@ -41,5 +44,21 @@ public class TempController {
             return r[1];
         }
         return "error";
+    }
+    @GetMapping("/BOJSubmit/{username}/{pNum}")
+    HashMap<String, String> BOJSubmit(@PathVariable("username") String username, @PathVariable("pNum") String p_num) {
+        WebClient webClient = WebClient.builder().build();
+
+        String url = "https://www.acmicpc.net/status?user_id="+username+"&problem_id=+"+p_num;
+        String html = webClient.get().uri(url).retrieve().bodyToMono(String.class).block();
+
+        HashMap<String, String> dto = new HashMap<>();
+        Document document = Jsoup.parse(html);
+        dto.put("id", document.select("td").first().text());
+        dto.put("result", document.select("td.result").first().text());
+        dto.put("memory", document.select("td.memory").first().text());
+        dto.put("time", document.select("td.time").first().text());
+
+        return dto;
     }
 }
